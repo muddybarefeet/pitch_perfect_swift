cf622a7c1ec01a2e0cf8bea5bfb22fca353a2c23
Anna Rogers <2muddybarefeet@gmail.com>
2016-06-21 07:54:42 -0700

    initial commit

--- PitchPerfet/ViewController.swift
+++ PitchPerfet/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    @IBOutlet weak var recordingLabel: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RecordAudio(sender: AnyObject) {
+        print("Record Button Pressed")
+        recordingLabel.text = "Recording!"
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("Stop Recording")
+    }
 }
 
